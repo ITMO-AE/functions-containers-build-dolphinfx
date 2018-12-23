37962b1bb63bcdea8ecc0da0b93efb7336fb0245
dolphinfx <45944922+dolphinfx@users.noreply.github.com>
2018-12-23 20:52:31 +0300

    Add files via upload

---
+++ task5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+void Reverse(vector <int>&v){
+    int z=0,b=v.size();
+
+    reverse(v.begin(),v.end());
+
+}
+
+
+int main(int argc, char *argv[])
+{
+    vector <int> a={1,2,3,4,5,6};
+
+    cout << "Вектор до поворота" << endl;
+
+    for (int i=0;i<a.size();i++){
+        cout<<a.at(i)<<" ";
+    }
+    cout<<endl;
+    Reverse(a);
+    cout << "Вектор после поворота" << endl;
+
+    for (int i=0;i<a.size();i++)
+    {
+        cout<<a.at(i)<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
